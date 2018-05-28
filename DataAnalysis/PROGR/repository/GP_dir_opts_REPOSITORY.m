75cb71c8a89c23a21c98bf81d5e7930e2304445f
Unknown <fedbarban+github@gmail.com>
2018-05-28 10:32:59 +0200

    Added differential path
    Switch to set different paths for diferent users

--- DataAnalysis/PROGR/repository/GP_dir_opts_REPOSITORY.m
+++ DataAnalysis/PROGR/repository/GP_dir_opts_REPOSITORY.m
@@ -3,18 +3,28 @@ function dir_opts = GP_dir_opts_REPOSITORY(localPath)
 %
 % localPath: subfolders path
 %
-% Marianna Semprini 
+% Marianna Semprini
 % IIT, April 2018
 
 if nargin <1
     localPath = '';
 end
 
-dirs.root_raw = 'C:\Users\MSemprini\Documents\_WORK\projects\GOSSWEILER\DATA\PILOT\RAWDATA';    % root data for raw acquisitions                                                             % root raw data
-dirs.root_data = 'C:\Users\MSemprini\Documents\_WORK\projects\GOSSWEILER\DATA\PILOT\analysis';  % root data for analysis
-% dirs.root_plot = 'Y:\SW_&_DATA\BIMFERR_MARI\DATA\figures';                                    % root data for images
+User=getenv('username');
+switch(User)
+    case 'MSemprini'
+        dirs.root_raw = 'C:\Users\MSemprini\Documents\_WORK\projects\GOSSWEILER\DATA\PILOT\RAWDATA';    % root data for raw acquisitions                                                             % root raw data
+        dirs.root_data = 'C:\Users\MSemprini\Documents\_WORK\projects\GOSSWEILER\DATA\PILOT\analysis';  % root data for analysis
+        % dirs.root_plot = 'Y:\SW_&_DATA\BIMFERR_MARI\DATA\figures';                                    % root data for images
+        
+    case 'Fede'
+        dirs.root_raw = 'C:\Users\Fede\Documents\Eperiments\Goss';    % root data for raw acquisitions                                                             % root raw data
+        dirs.root_data = 'C:\Users\Fede\Documents\Eperiments\Goss\behavior';  % root data for analysis
+        % dirs.root_plot = 'Y:\SW_&_DATA\BIMFERR_MARI\DATA\figures';                                    % root data for images
+        
+end
 
-dir_opts.dir_rawEEGData = fullfile(dirs.root_raw, localPath, 'EEG_DATA');                       % contains raw EEG Data 
+dir_opts.dir_rawEEGData = fullfile(dirs.root_raw, localPath, 'EEG_DATA');                       % contains raw EEG Data
 dir_opts.dir_performance = fullfile(dirs.root_data, 'performance');                             % contains task performance Data
 % dir_opts.dir_EEGData = fullfile(dirs.root_data, 'EMG');                             % contains processed EEG data
 % dir_opts.dir_TASKData = fullfile(dirs.root_data, 'KIN');                            % contains processed performance data
